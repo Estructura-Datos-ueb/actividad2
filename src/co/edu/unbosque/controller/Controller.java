b4aecd79b690065a9eabcfa579c94a490ebb7faf
estebanCastr0 <83050986+estebanCastr0@users.noreply.github.com>
2021-09-06 12:12:24 -0500

    Se crean las clases principales para su funcionamienton con mvc

--- src/co/edu/unbosque/controller/Controller.java
+++ src/co/edu/unbosque/controller/Controller.java
@@ -1,4 +1,20 @@
 package co.edu.unbosque.controller;
+import co.edu.unbosque.view.ViewConsole;
+import co.edu.unbosque.model.pilasColas;
 
 public class Controller {
+
+    private ViewConsole v;
+    private pilasColas pc;
+
+    public Controller(){
+        v = new ViewConsole();
+        pc = new pilasColas();
+        ejecucion();
+    }
+
+    public void ejecucion(){
+
+        v.mostrarInformacion("prueba numero 1");
+    }
 }
--- src/co/edu/unbosque/view/ViewConsole.java
+++ src/co/edu/unbosque/view/ViewConsole.java
@@ -1,4 +1,45 @@
 package co.edu.unbosque.view;
 
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
 public class ViewConsole {
+
+    private String tamano;
+
+    public ViewConsole() {
+
+    }
+    public void ingresarinformacion(String mensaje){
+        tamano =  JOptionPane.showInputDialog(mensaje);
+    }
+
+    public void mostrarInformacion( String mensaje) {
+        JOptionPane.showMessageDialog(null, mensaje);
+    }
+
+    public Integer mostrarOpcion(String mensaje) {
+        return JOptionPane.showConfirmDialog(null, mensaje);
+    }
+
+    public String leerDato(String mensaje) {
+        String opcion = "";
+        opcion = JOptionPane.showInputDialog(mensaje);
+
+        return opcion;
+    }
+
+    public String getTamano() {
+        return tamano;
+    }
+
+    public void setTamano(String tamano) {
+        this.tamano = tamano;
+    }
+
+
+
+
+
 }
